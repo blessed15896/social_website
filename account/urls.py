e7cf5f942e9ad72ead35b8c2f674ecebd48218d8
Blessed <blessed15896@gmail.com>
2024-06-25 19:30:58 +0200

    Implement user registration

--- account/urls.py
+++ account/urls.py
@@ -2,9 +2,7 @@ from django.urls import path, include
 from account import views
 
 urlpatterns = [
-    # auth urls
     path('', include('django.contrib.auth.urls')),
-
-    # dashboard url
     path('', views.dashboard, name='dashboard'),
+    path('register/', views.register, name='register'),
 ]
