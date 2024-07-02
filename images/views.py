cdb7d1b4f79cd31d0026dc736c740a2df967e9ee
Blessed <blessed15896@gmail.com>
2024-07-02 13:16:06 +0200

    Create a JavaScript bookmarklet to download images from other sites

--- images/views.py
+++ images/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Image
+from .forms import ImageCreateForm
 
-# Create your views here.
+
+@login_required
+def image_create(request):
+    if request.method == 'POST':
+        form = ImageCreateForm(data=request.POST)
+        if form.is_valid():
+            new_image = form.save(commit=False)
+            new_image.user = request.user
+            new_image.save()
+            messages.success(request, 'Image added created')
+            return redirect(new_image.get_absolute_url())
+    else:
+        form = ImageCreateForm(data=request.GET)
+    return render(request, 'images/image/create.html',
+                  {'section': 'images', 'form': form})
+
+
+def image_detail(request, id, slug):
+    image = get_object_or_404(Image, id=id, slug=slug)
+    return render(request, 'images/image/detail.html',
+                  {'section': 'images', 'image': image})
